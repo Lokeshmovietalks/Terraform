711aeabf9495561927437eaf4a6597c955ccbd68
Lokeshmovietalks <paspuletireddylokesh@gmail.com>
2025-05-10 11:03:23 +0530

    day3_1

---
+++ Day_3/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value ="ami-0822a7a2356687b0f"
+instance_type = "t2.micro"
---
+++ Day_3/variables.tf
@@ -0,0 +1,6 @@
+variable "ami_value"{
+    description = "value of ami id"
+}
+variable "instance_type"{
+    description = "value of instance type"
+}
