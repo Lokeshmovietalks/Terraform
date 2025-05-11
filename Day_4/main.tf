c5184f24bb95793abb468ece21a9c9ca09b4bdee
Lokeshmovietalks <paspuletireddylokesh@gmail.com>
2025-05-11 20:53:50 +0530

    Day_4

--- Day_4/main.tf
+++ Day_4/main.tf
@@ -5,3 +5,13 @@ provider "aws"{
 resource "aws_s3_bucket" "StateFile" {
   bucket = "statefiledemo"
 }
+
+resource "aws_dynamodb_table" "Lock" {
+  name = "terraform_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
